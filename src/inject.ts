669e4590d8e2648aee51543d2d7cd13aa616b2f5
tristan <tristan.lee@qq.com>
2021-08-13 22:02:41 +0800

    fix injection

--- src/inject.ts
+++ src/inject.ts
@@ -6,14 +6,19 @@ const { HttpProvider } = require('@polkadot/rpc-provider');
 
 const SCHEMA_PATH = path.join(__dirname, './', 'schema.json');
 const START_BLOCK = process.env.START_BLOCK ? parseInt(process.env.START_BLOCK, 10) : 0
-const END_BLOCK = process.env.END_BLOCK ? parseInt(process.env.END_BLOCK, 10) : START_BLOCK+5
+const END_BLOCK = process.env.END_BLOCK ? parseInt(process.env.END_BLOCK, 10) : START_BLOCK+10
 const RPC_URL = process.env.RPC_URL || 'http://127.0.0.1:9933'
 const NEW_URL = process.env.NEW_URL || 'http://127.0.0.1:9934'
 
 const provider = new HttpProvider(NEW_URL);
 var web3 = new Web3(new Web3.providers.HttpProvider(RPC_URL));
+var new_web3 = new Web3(new Web3.providers.HttpProvider(NEW_URL));
 //var utils = web3.utils;
 var mvs = web3.eth;
+var frontier = new_web3.eth;
+
+const sleep = (ms: number): Promise<void> =>
+  new Promise((resolve) => setTimeout(resolve, ms));
 
 async function main () {
 	// Create the API and wait until ready
@@ -46,16 +51,20 @@ async function main () {
 				console.log(tx.raw)
 				index++
 
-				mvs.sendSignedTransaction(tx.raw)
+				// shouldn't await coz using manual-seal
+				frontier.sendSignedTransaction(tx.raw)
 			        .on('receipt', console.log)
 			        .catch(console.log);
 			}
 			console.log('--------------')
+			// sleep 200ms for tx to be surely injected into block
+			await sleep(200);
 		} 
 		
 		let ret = await api.rpc.engine.createBlock(true, false)
 		console.log(`seal block hash: ${ret.hash}`)
 		blockNumber++
+
 	}
 }
 
